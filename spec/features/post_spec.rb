fb1596f3f1cfe42fa5def528c99e2921bd0b1d2a
Colleen Schnettler <leenyburger@users.noreply.github.com>
2018-04-25 12:07:00 -0400

    Fixed spacing issues and updated link on nav bar

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -5,25 +5,25 @@ describe 'navigate' do
     @user = FactoryGirl.create(:user)
     login_as(@user, :scope => :user)
   end
-	describe 'index' do
+  describe 'index' do
     before do
       visit posts_path
     end
-		it 'can be reached successfully' do
-			expect(page.status_code).to eq(200)
-		end
-
-		it 'has content of Posts' do
-			expect(page).to have_content(/Posts/)
-		end
-
-		it 'has a list of posts' do
-			post1 = FactoryGirl.build_stubbed(:post)
-			post2 = FactoryGirl.build_stubbed(:second_post)
-			visit posts_path
-			expect(page).to have_content(/Rationale|content/)
+    it 'can be reached successfully' do
+		  expect(page.status_code).to eq(200)
     end
-	end
+
+    it 'has content of Posts' do
+		  expect(page).to have_content(/Posts/)
+    end
+
+    it 'has a list of posts' do
+      post1 = FactoryGirl.build_stubbed(:post)
+		  post2 = FactoryGirl.build_stubbed(:second_post)
+		  visit posts_path
+		  expect(page).to have_content(/Rationale|content/)
+    end
+  end
 
   describe 'new' do
     it 'has a link from the homepage' do
@@ -31,31 +31,29 @@ describe 'navigate' do
       click_link("new_post_from_nav")
       expect(page.status_code).to eq(200)
     end
-
-
   end
 
 	describe 'creation' do
-		before do
-			visit new_post_path
-		end
+    before do
+		  visit new_post_path
+    end
 
-		it 'has a new form that can be reached' do
-			expect(page.status_code).to eq(200)
-		end
+    it 'has a new form that can be reached' do
+		  expect(page.status_code).to eq(200)
+    end
 
-		it 'can be created from new form page' do
-			fill_in 'post[date]', with: Date.today
-			fill_in 'post[rationale]', with: "Some rationale"
-			click_on "Save"
-			expect(page).to have_content("Some rationale") #This is the show page
+    it 'can be created from new form page' do
+		  fill_in 'post[date]', with: Date.today
+		  fill_in 'post[rationale]', with: "Some rationale"
+		  click_on "Save"
+		  expect(page).to have_content("Some rationale") #This is the show page
     end
 
     it 'will have a user associated with it' do
-    	fill_in 'post[date]', with: Date.today
-			fill_in 'post[rationale]', with: "User Association"
-			click_on "Save"
-			expect(User.last.posts.last.rationale).to eq("User Association")
+      fill_in 'post[date]', with: Date.today
+		  fill_in 'post[rationale]', with: "User Association"
+		  click_on "Save"
+		  expect(User.last.posts.last.rationale).to eq("User Association")
     end
 	end
 
