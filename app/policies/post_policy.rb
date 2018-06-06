c15d2b28e79e9e63741f409a0664eae3f17a7f87
Colleen Schnettler <leenyburger@users.noreply.github.com>
2018-06-06 11:08:16 -0400

    Added labels

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -2,6 +2,22 @@ class PostPolicy < ApplicationPolicy
   #This update method overrides update in application policy.
   #Pundit gives access to record and user from Application Policy. Here the record is the post.
   def update?
-    record.user_id == user.id || admin_types.include?(user.type)
+    return true if post_approved? && admin?
+    return true if user_or_admin && !post_approved?
   end
+
+  private
+
+    def user_or_admin
+      record.user_id == user.id || admin?
+    end
+
+    def admin?
+      admin_types.include?(user.type)
+    end
+
+    def post_approved?
+      record.approved?
+    end
+
 end
