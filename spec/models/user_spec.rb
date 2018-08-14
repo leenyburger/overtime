83bdc40cf2bb1d3ddea738bfb9660ac70004e016
Colleen Schnettler <leenyburger@users.noreply.github.com>
2018-08-13 20:00:29 -0400

    End of lecture 67

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,19 +9,32 @@ RSpec.describe User, type: :model do
     it "can be created" do
       expect(@user).to be_valid
     end
+  end
+
+  describe "validations" do
 
     it "cannot be created without first_name" do
       @user.first_name = nil
       expect(@user).to_not be_valid
     end
 
-    it "cannot be created without ast_name" do
+    it "cannot be created without last_name" do
       @user.last_name = nil
       expect(@user).to_not be_valid
     end
 
     it "cannot be created without phone" do
       @user.phone = nil
+      expect(@user).to_not be_valid
+    end
+
+    it "requires the phone attr to only contain integers" do
+      @user.phone = 'mygreatstr'
+      expect(@user).to_not be_valid
+    end
+
+    it "requires the phone attr to only have 10 chars" do
+      @user.phone = '1234567891011'
       expect(@user).to_not be_valid
     end
   end
